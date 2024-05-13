398c7b48bc452eb75767c0492b2d839e958943fb
Dmitry Oleynikov <None>
2024-05-13 10:55:01 +0300

    logs

---
+++ logs/main.js
@@ -0,0 +1,33 @@
+// const username = 'admin'
+// const password = 'fxw7ZvwWx@35112!'
+
+// const urlSrv = 'https://10.100.104.144:8088/deviceManager/ibase/i18n/i18n_resource.js?t=1544105949486'
+
+// const xhr = new XMLHttpRequest()
+
+// xhr.open('GET', 'https://10.100.104.144:8088/deviceManager/ibase/i18n/i18n_resource.js?t=1544105949486', true)
+// xhr.onload = () => {
+//     if (xhr.status >= 200 && xhr.status < 300){
+//         console.log('Good!', xhr.responseText)
+//     } else {
+//         console.error('Error', xhr.statusText)
+//     }
+// }
+// xhr.onerror = () => {
+//     console.error('Network Error')
+// }
+
+// xhr.send()
+
+// // const auth = {
+// //         usernamename: username,
+// //         password: password
+// // }
+
+// // axios.get(urlSrv, auth)
+// //     .then(response => {
+// //         console.log('responce.data')
+// //     })
+// //         .catch(error => {
+// //             console.error('Error fetching logs', error)
+// //         })
